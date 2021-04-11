e26f995aa776eb1235dfa45168a110d8a05c895a
Azamat Gaparov <agaparov@relaynetwork.com>
2021-04-11 19:00:15 -0400

    latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-recipe-prj-start';
+  loadedFeature = 'recipe'
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
